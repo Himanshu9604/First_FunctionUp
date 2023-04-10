e63f7322979424d55cbc2837aed26b3984c7ce4a
Himanshu <himanshuyadav201000@gmail.com>
2023-04-10 21:41:48 +0530

    react-masterclass-first-task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
 import './App.css';
+import First from './Assignment1/First';
+// import Second from './Assignment2/Second';
+// import Third from './Assignment3/Third';
 
 function App() {
   return (
     <div className="App">
-      
+      <First/>
+      {/* <Second/> */}
+      {/* <Third/> */}
     </div>
   );
 }
---
+++ src/Assignment3/Third.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import styles from './third.module.css'; 
+
+function Third() {
+    const [greet, setGreet] = useState(['Happy Birthday','Happy Anneversary', 'Good Morning','Good Afternoon','Good Evening'])
+    const [currentNum, setCurrentNum] = useState(0)
+    
+    const handleGreet = () => {
+        setCurrentNum(currentNum + 1)
+    }
+     
+    return (
+        <div>
+            <h1 style={{backgroundColor:'yellow'}} className={styles.greetings}>Greetings : {greet[currentNum]}</h1> {/* Use class name from module CSS */}
+            <button onClick={handleGreet}>Greet</button>
+        </div>
+    )
+}
+
+export default Third;
