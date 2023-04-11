591c5ef6f5cccefa1bad19e037ea7140cf2a480d
Himanshu <himanshuyadav201000@gmail.com>
2023-04-11 21:33:31 +0530

    Day-2 -First-Task

--- src/Assignment2/Second.js
+++ src/Assignment2/Second.js
@@ -1,43 +1,139 @@
-import React, { useState } from 'react';
-import styles from './Second.module.css'; 
+import React, { useRef, useState } from "react";
+import style from "./Second.module.css";
 
 function Second() {
-  const [times, setTimes] = useState([]);
-  function onmouseover(event){
-    const name = event.target.getAttribute('data-name');
-    setTimes([...times,{time: new Date().toLocaleString() ,eventmouse:'onMouseOver',id:name}]);
+  let [firstname, setFirstName] = useState("");
+  let [lastname, setLastName] = useState("");
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  let [arr, setArr] = useState(
+    JSON.parse(localStorage.getItem("userData")) || []
+  );
+  let [refer, setRefer] = useState("");
 
-    
-}
-function onmouseout(event){
-    // Define function logic here
-    const name = event.target.getAttribute('data-name');
-    setTimes([...times,{time: new Date().toLocaleString() ,eventmouse:'onMouseOut',id:name}]);
+  const firstRef = useRef("");
 
+  function handleDelete(firstName) {
+    setArr(arr.filter((ele) => ele.firstName != firstName));
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let newData = [
+      ...arr,
+      {
+        firstName: firstname,
+        lastName: lastname,
+        Email: email,
+        Password: password,
+      },
+    ];
+    setArr(newData);
+    localStorage.setItem("userData", JSON.stringify(newData));
+    setRefer(firstname);
   }
   return (
     <div>
-      <h1 className={styles.title} data-name="Header 1" onMouseOver={onmouseover} onMouseOut={onmouseout}>H1</h1>
-      <h2 className={styles.title} data-name="Header 2" onMouseOver={onmouseover} onMouseOut={onmouseout}>H2</h2>
-    <div className={styles.title} data-name="div" onMouseOver={onmouseover} onMouseOut={onmouseout}>Div Tag</div>
-      <table className={styles.table}>
-        <thead>
-          <tr>
-            <th>Time</th>
-            <th>Event Name</th>
-            <th>Tag Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {times.map((log, index) => (
-            <tr key={index}>
-              <td>{log.time}</td>
-              <td>{log.eventmouse}</td>
-              <td>{log.id}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <section className={style.mainDiv}>
+        <div className={style.firstSection}>
+          <div className={style.firstHalf}>
+            <div className={style.form}>
+              <form onSubmit={handleSubmit}>
+                <label>First Name</label>
+                <input
+                  type="text"
+                  value={firstname}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+                <label>Last Name</label>
+                <input
+                  type="text"
+                  value={lastname}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+                <label>Email</label>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <label>Password</label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <br />
+                <button className={style.btnn}>Register</button>
+              </form>
+            </div>
+          </div>
+          <div className={style.secondHalf}>
+            <table className={style.table}>
+              <thead>
+                <tr>
+                  <th className={style.th}>First Name</th>
+                  <th className={style.th}>Last Name</th>
+                  <th className={style.th}>Email</th>
+                  <th className={style.th}>Password</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className={style.td}>{firstname}</td>
+                  <td className={style.td}>{lastname}</td>
+                  <td className={style.td}>{email}</td>
+                  <td className={style.td}>{password}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="secondSection">
+          <table className="table">
+            <thead>
+              <tr>
+                <th className={style.th}>srno</th>
+                <th className={style.th}>First Name</th>
+                <th className={style.th}>Last Name</th>
+                <th className={style.th}>Email</th>
+                <th className={style.th}>Delete</th>
+                <th className={style.th}>Update</th>
+              </tr>
+            </thead>
+            <tbody>
+              {arr.map((users, index) => {
+                return (
+                  <>
+                    <tr key={index}>
+                      <td className={style.td}>{index + 1}</td>
+                      <td className={style.td}>{users.firstName}</td>
+                      <td className={style.td}>{users.lastName}</td>
+                      <td className={style.td}>{users.Email}</td>
+
+                      <td className={style.td}>
+                        <button
+                          className={style.delete}
+                          onClick={() => handleDelete(users.firstName)}
+                        >
+                          delete
+                        </button>{" "}
+                      </td>
+                      <td className={style.td}>
+                        <button
+                          className={style.edit}
+                          onClick={() => handleDelete(index)}
+                        >
+                          Edit
+                        </button>
+                      </td>
+                    </tr>
+                  </>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </section>
     </div>
   );
 }
