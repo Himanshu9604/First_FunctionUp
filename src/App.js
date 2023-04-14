6793b90777ccc870cb600c0610eb44ea0ebfcb3e
Himanshu <himanshuyadav201000@gmail.com>
2023-04-14 22:50:12 +0530

    Routing-Assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import  'react-router-dom'
+import {Route,Routes} from 'react-router-dom'
+import Home from './Home/Home.js'
+import Contact from './Contact/Contact.js'
+import NavBar from './NavBar/NavBar.js'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/Contact' element={<Contact/>}/>
+      </Routes>
     </div>
   );
 }
