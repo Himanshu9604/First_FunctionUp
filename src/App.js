ecdb78b496f0d77b370ffc1a569fea84464213a6
Himanshu <himanshuyadav201000@gmail.com>
2023-05-02 06:42:49 +0530

    Extra Assignment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-// import First from './Hooks/Usestate'
-
 import Bonus from "./OtherProjects/Bonus";
-import Trends from "./RightSide/First";
-import First from "./Useeffect/First";
-import Second from "./Useeffect/Second";
 
-// import Timer from "./Hooks/Timer";
-
-// import Second from "./Hooks/LuckyNumber";
 function App() {
   return (
     <div className="App">
-      {/* <First/>     */}      
-      {/* <Second/> */}
-      {/* <Timer/> */}
-      {/* <First/> */}
-      {/* <Second/> */}
-      {/* <First/> */}
-      {/* <New/>
-       */}
-       {/* <Trends/> */}
-       <Bonus/>
+          <Bonus/>
     </div>
   );
 }
